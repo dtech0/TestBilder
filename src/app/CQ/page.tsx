b82b5c5fee095bcf9d6c4755015d2ef3306d751b
dtech0 <sayedabdullahal11@gmail.com>
2025-07-28 21:08:00 +0600

    error for vercel

--- src/app/CQ/page.tsx
+++ src/app/CQ/page.tsx
@@ -1,4 +1,41 @@
+// "use client"
+
+// import { useRef } from "react"
+// import { useSearchParams } from "next/navigation"
+
+// export default function IframeCQ() {
+//   const iframeRef = useRef<HTMLIFrameElement>(null)
+//   const searchParams = useSearchParams()
+//   const templateId = searchParams.get("templateId")
+
+//   const handlePrint = () => {
+//     if (iframeRef.current && iframeRef.current.contentWindow) {
+//       iframeRef.current.contentWindow.print()
+//     }
+//   }
+
+//   if (!templateId) {
+//     return <div className="text-red-600 text-center mt-10">Template ID missing!</div>
+//   }
+
+//   return (
+//     <div className="w-full justify-items-center">
+//       <div>
+//         <iframe ref={iframeRef} src={`/demoCQ/CQ/cq.html?templateId=${templateId}`} width="835" height="750" />
+//       </div>
+//       <div>
+//         <button
+//           onClick={handlePrint}
+//           className="fixed z-100 bg-black text-white py-1 w-[100px] font-bold rounded-xl hover:bg-blue-700 bottom-10 mx-[-50px]"
+//         >
+//           Print
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
 "use client"
+export const dynamic = "force-dynamic"
 
 import { useRef } from "react"
 import { useSearchParams } from "next/navigation"
@@ -21,7 +58,13 @@ export default function IframeCQ() {
   return (
     <div className="w-full justify-items-center">
       <div>
-        <iframe ref={iframeRef} src={`/demoCQ/CQ/cq.html?templateId=${templateId}`} width="835" height="750" />
+        <iframe
+          ref={iframeRef}
+          src={`/demoCQ/CQ/cq.html?templateId=${templateId}`}
+          width="835"
+          height="750"
+          title="CQ Template"
+        />
       </div>
       <div>
         <button
